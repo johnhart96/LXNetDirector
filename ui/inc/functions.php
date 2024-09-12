d31f081517fe1b688e150a32586a2981fc664dea
johnhart96 <johnhart96@outlook.com>
2024-09-12 16:52:18 +0100

    menuitem

--- ui/inc/functions.php
+++ ui/inc/functions.php
@@ -2,4 +2,15 @@
 function base( $l = NULL ) {
     return "http://" . $_SERVER['SERVER_NAME'] . "/" . $l;
 }
+
+function menu_item( $link , $text ) {
+    $location = filter_var( $_GET['url'] , FILTER_UNSAFE_RAW );
+    if( $link == $location ) {
+        $active = "active";
+    } else {
+        $active = "";
+    }
+    return '<li class="nav-item"><a class="nav-link ' . $active . '" aria-current="page" href="/' . $link . '">' . $text . '</a></li>';
+}
+
 ?>
