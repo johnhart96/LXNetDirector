dc58c09db6ca8595ebcb21abc822c7766ba5ccb3
johnhart96 <johnhart96@outlook.com>
2024-09-12 16:04:09 +0100

    Adding bootstrap

--- ui/index.php
+++ ui/index.php
@@ -11,8 +11,12 @@
     <head>
         <title>LXNetDirector</title>
         <link href="css/styles.css" rel="stylesheet" type="text/css" />
+        <link href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
     </head>
     <body>
-        
+        <div class="container">
+            <h1>Hello</h1>
+        </div>
     </body>
 </html>
