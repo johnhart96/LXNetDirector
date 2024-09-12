f482a368d635369f22f962742499e4a660640a37
johnhart96 <johnhart96@outlook.com>
2024-09-12 15:59:58 +0100

    Basic structure

--- ui/index.php
+++ ui/index.php
@@ -4,7 +4,15 @@
 * Author: johnhart96
 * Version 1
 * Licence: Apache 2.0
-# Repo: https://github.com/johnhart96/LXNetDirector
-*
+* Repo: https://github.com/johnhart96/LXNetDirector
 */
 ?>
+<html>
+    <head>
+        <title>LXNetDirector</title>
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        
+    </body>
+</html>
