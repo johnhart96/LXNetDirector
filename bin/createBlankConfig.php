2cf7fc54046fc308b812fd6128034b5eaa9080c5
johnhart96 <johnhart96@outlook.com>
2024-09-13 11:31:45 +0100

    Allow to create blank config

--- bin/createBlankConfig.php
+++ bin/createBlankConfig.php
@@ -1,4 +1,4 @@
 <?php
 define( "CONFIG_PATH" , "/usr/local/lx_network/shares/config/LXNetDirector.db" );
-$create = new SQLite3( CONFIG_PATH );
+$create = new SQLite3( CONFIG_PATH , SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE );
 ?>
