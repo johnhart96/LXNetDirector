a5335fa7023a94a7ed6e27bfd448a6adff8203f2
johnhart96 <johnhart96@outlook.com>
2024-09-13 10:12:03 +0100

    Show site and installer info on dashboard

--- ui/dashboard.php
+++ ui/dashboard.php
@@ -21,15 +21,45 @@
         <div class="card">
             <div class="card-header"><strong>Site info:</strong></div>
             <div class="card-body">
-                sdfsdf
+                <table class="table table-bordered table-striped">
+                    <tbody>
+                        <tr>
+                            <th width="25%">Site name:</th>
+                            <td><?php echo read_setting( "site_name" ); ?></td>
+                        </tr>
+                        <tr>
+                            <th width="25%">Address:</th>
+                            <td><?php echo read_setting( "site_address" ); ?></td>
+                        </tr>
+                        <tr>
+                            <th width="25%">Telephone number:</th>
+                            <td><?php echo read_setting( "site_telephone" ); ?></td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
     <div class="col-sm">
         <div class="card">
-            <div class="card-header"><strong>Supplier info:</strong></div>
+            <div class="card-header"><strong>Installer info:</strong></div>
             <div class="card-body">
-                sdfsdf
+                <tr>
+                    <th width="25%">Installer name:</th>
+                    <td><?php echo read_setting( "installer_name" ); ?></td>
+                </tr>
+                <tr>
+                    <th width="25%">Project no:</th>
+                    <td><?php echo read_setting( "installer_project" ); ?></td>
+                </tr>
+                <tr>
+                    <th width="25%">Contact telephone:</th>
+                    <td><?php echo read_setting( "installer_telephone" ); ?></td>
+                </tr>
+                <tr>
+                    <th width="25%">Contact email:</th>
+                    <td><?php echo read_setting( "installer_email" ); ?></td>
+                </tr>
             </div>
         </div>
     </div>
