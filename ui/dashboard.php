17b6aee2f16835db30a57982edd63b46bae97ac9
johnhart96 <johnhart96@outlook.com>
2024-09-13 09:28:01 +0100

    Starting to design a dashboard

--- ui/dashboard.php
+++ ui/dashboard.php
@@ -1,5 +1,14 @@
 <div class="row border-bottom">
     <div class="col">
-        <h1>Welcome, <?php echo user(); ?></h1>
+        <h1>Welcome, <?php echo ucFirst( user() ); ?></h1>
+    </div>
+</div>
+<div class="row">
+    <div class="col">
+        <h2>Health</h2>
+        <?php
+        $memory = shell( "free -h" );
+        echo $memory;
+        ?>
     </div>
 </div>
