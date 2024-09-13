e632ab9bf46554311a4a33f5c8532f2851a985a6
johnhart96 <johnhart96@outlook.com>
2024-09-13 10:42:25 +0100

    Starting setting page layout

--- ui/settings.php
+++ ui/settings.php
@@ -3,3 +3,15 @@
         <h1>Settings</h1>
     </div>
 </div>
+
+<div class="row">
+    <div class="col">
+        <form method="post">
+            <h2>Site info:</h2>
+            <div class="form-group">
+                <label for="site_name">Site name:</label>
+                <input name="site_name" value="<?php echo read_setting( "site_name" ); ?>" class="form-control">
+            </div>
+        </form>
+    </div>
+</div>
