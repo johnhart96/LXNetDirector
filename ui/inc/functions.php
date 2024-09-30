44a58670c278b569db66dd09a1559f01cf14b623
johnhart96 <johnhart96@outlook.com>
2024-09-30 16:32:13 +0100

    Create config directly in web ui

--- ui/inc/functions.php
+++ ui/inc/functions.php
@@ -1,8 +1,28 @@
 <?php
-define( "CONFIG_PATH" , "/usr/local/lx_network/shares/config/LXNetDirector.db" );
+define( "CONFIG_PATH" , "../usr/LXNetDirector.db" );
 // Create db if do not exist
+if( ! file_exists( "../usr" ) ) {
+    mkdir( "../usr" );
+}
 if( ! file_exists( CONFIG_PATH ) ) {
     $create = new SQLite3( CONFIG_PATH );
+    $create->query("
+        PRAGMA foreign_keys = off;
+        BEGIN TRANSACTION;
+
+        -- Table: logs
+        CREATE TABLE IF NOT EXISTS logs (id INTEGER PRIMARY KEY AUTOINCREMENT, stamp DATETIME CURRENT_TIMESTAMP, device TEXT NOT NULL, mode TEXT NOT NULL, entryLog TEXT NOT NULL);
+
+        -- Table: settings
+        CREATE TABLE IF NOT EXISTS settings (id INTEGER PRIMARY KEY AUTOINCREMENT, setting TEXT NOT NULL, setting_value TEXT NOT NULL);
+
+        -- Table: shares
+        CREATE TABLE IF NOT EXISTS shares (id INTEGER PRIMARY KEY AUTOINCREMENT, share_name TEXT NOT NULL, writable TEXT DEFAULT 'yes', guests TEXT DEFAULT 'yes', path TEXT NOT NULL);
+
+        COMMIT TRANSACTION;
+        PRAGMA foreign_keys = on;
+
+    ");
 }
 
 function base( $l = NULL ) {
